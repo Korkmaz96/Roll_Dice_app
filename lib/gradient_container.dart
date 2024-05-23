1378e3a7f4a7d1be1b6b5ea3f5c3416534874a2f
Mikail Korkmaz <mikail-korkmaz@web.de>
2024-05-23 14:12:26 +0200

    More Custom Widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,33 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:second_app/styled_text.dart';
+ 
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradiantContainer extends StatelessWidget {
-  const GradiantContainer({super.key}); 
-  
+  const GradiantContainer({super.key});
 
   @override
   Widget build(context) {
-
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 237, 236, 240),
-                Color.fromARGB(255, 50, 2, 87),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Upcoming Roll_Dice_App',
-              style: TextStyle(
-                color: Colors.white, 
-                fontSize: 28,
-                ),
-            ),
-          ),
-        );
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: const [
+            Color.fromARGB(255, 237, 236, 240),
+            Color.fromARGB(255, 50, 2, 87),
+          ],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
   }
-  
 }
